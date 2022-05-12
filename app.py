7aa2ad6b037539144b44c231072e73dc17bc4c0b
Rbiasuz <rodrigo_biasuz@hotmail.com>
2022-05-12 17:04:27 -0300

    check len line

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ with open(file,'r',encoding='latin-1') as f:
         except:
             pass
 
+new_list = []
+for line in lista:
+    if len(line) < 5:
+        print(line)
+    else:
+        new_list.append(line)
+
 #limpar texto
 #tokenizar
 
